113e45379b600dc1d6149a1b6c91ae584e072aae
Drew Maring <andrew.maring@gmail.com>
2019-10-15 16:29:49 -0700

    Add Login vue and logic for vuex to store logged-in user

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,55 +1,41 @@
 <template>
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-8">
-        <div class="card">
-          <div class="card-header">Login</div>
-          <div class="card-body">
-            <div v-if="error" class="alert alert-danger">{{error}}</div>
-            <form action="#" @submit.prevent="submit">
-              <div class="form-group row">
-                <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
-
-                <div class="col-md-6">
-                  <input
-                    id="email"
-                    type="email"
-                    class="form-control"
-                    name="email"
-                    value
-                    required
-                    autofocus
-                    v-model="form.email"
-                  />
-                </div>
-              </div>
-
-              <div class="form-group row">
-                <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
-
-                <div class="col-md-6">
-                  <input
-                    id="password"
-                    type="password"
-                    class="form-control"
-                    name="password"
-                    required
-                    v-model="form.password"
-                  />
-                </div>
-              </div>
-
-              <div class="form-group row mb-0">
-                <div class="col-md-8 offset-md-4">
-                  <button type="submit" class="btn btn-primary">Login</button>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
+    <form action="#" @submit.prevent="submit">
+    <div class="form-group row">
+      <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
+      <div class="col-md-6">
+        <input
+          id="email"
+          type="email"
+          class="form-control"
+          name="email"
+          value
+          required
+          autofocus
+          v-model="form.email"
+        />
       </div>
     </div>
-  </div>
+
+    <div class="form-group row">
+      <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
+
+      <div class="col-md-6">
+        <input
+          id="password"
+          type="password"
+          class="form-control"
+          name="password"
+          required
+          v-model="form.password"
+        />
+      </div>
+    </div>
+    <div class="form-group row mb-0">
+      <div class="col-md-8 offset-md-4">
+        <button type="submit" class="btn btn-primary">Login</button>
+      </div>
+     </div>
+    </form>
 </template>
 
 <script>
@@ -71,7 +57,7 @@ export default {
         .auth()
         .signInWithEmailAndPassword(this.form.email, this.form.password)
         .then(data => {
-          this.$router.replace({ name: "Dashboard" });
+          this.$router.replace({ name: "swiping" });
         })
         .catch(err => {
           this.error = err.message;
