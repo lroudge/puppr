b47617571fa0120e66c1552264b1615fca662139
Laura Roudge <laura.derohan@gmail.com>
2019-11-05 14:17:18 -0800

    Add comments and semi-colon in all the non-components Vue files

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,15 @@
     </div>
 </template>
 <script>
-
-
     export default {
         data() {
             return {
+                // This determines if we should slide to the left or the right, depending on the route
                 transitionName: "slide-left"
             }
         },
+        // These two functions change the transition name between left and right
+        // Depending on which route it's coming from and which route it's going to
         beforeRouteUpdate (to, from, next) {
             const toPath = to.path;
             const fromPath = from.path;
@@ -28,7 +29,7 @@
             setTimeout(function() { next()}, 1000);
         },
         watch: {
-            '$route' (to, from, next) {
+            '$route' (to, from) {
                 const toPath = to.path;
                 const fromPath = from.path;
                 if (fromPath === '/swiping' && toPath === '/myprofile')
@@ -39,7 +40,6 @@
                     this.transitionName = 'slide-right';
                 else
                     this.transitionName = 'slide-left';
-                //setTimeout(function() { next()}, 1000);
             }
         }
     };
--- src/store.js
+++ src/store.js
@@ -1,65 +1,69 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { db } from './main';
+import {db} from './main';
 import createPersistedState from 'vuex-persistedstate';
 
-Vue.use(Vuex)
+// Tell Vue to use the Vuex store
+Vue.use(Vuex);
 
+// Initialize and export store
 export default new Vuex.Store({
-  plugins: [createPersistedState()],
-  name: 'store',
-  state: {
-    user: {
-      loggedIn: false,
-      data: null,
-      profile: null
-    }
-  },
+    plugins: [createPersistedState()],
+    name: 'store',
+    state: {
+        user: {
+            loggedIn: false,
+            data: null,
+            profile: null
+        }
+    },
+    getters: {
+        // This makes it able to access the current logged in user data
+        user(state) {
+            return state.user
+        }
+    },
+    mutations: {
+        SET_LOGGED_IN(state, value) {
+            state.user.loggedIn = value
+        },
+        SET_USER(state, data) {
+            state.user.data = data
+        },
+        SET_PROFILE(state, profile) {
+            state.user.profile = profile
+        }
+    },
+    actions: {
+        // Allows to fetch the current user's authentication data after they log in
+        fetchUser({commit}, user) {
+            commit('SET_LOGGED_IN', user !== null);
 
-  getters: {
-    user (state) {
-      return state.user
+            if (user) {
+                commit('SET_USER', {
+                    displayName: user.displayName,
+                    email: user.email,
+                    localId: user.uid
+                });
+                const ref = db.collection('users').doc(user.uid);
+                ref.get().then(function (doc) {
+                    commit('SET_PROFILE',
+                        doc.data()
+                    )
+                })
+            } else {
+                commit('SET_USER', null);
+                commit('SET_PROFILE', null)
+            }
+        },
+        // Sets the profile data of a user
+        fetchProfile({commit}, userUID) {
+            const ref = db.collection('users').doc(userUID);
+            ref.get().then(function (doc) {
+                commit('SET_PROFILE',
+                    doc.data()
+                )
+            })
+        }
     }
-  },
-  mutations: {
-    SET_LOGGED_IN (state, value) {
-      state.user.loggedIn = value
-    },
-    SET_USER (state, data) {
-      state.user.data = data
-    },
-    SET_PROFILE (state, profile) {
-      state.user.profile = profile
-    }
-  },
-  actions: {
-    fetchUser ({ commit }, user) {
-      commit('SET_LOGGED_IN', user !== null)
-
-      if (user) {
-        commit('SET_USER', {
-          displayName: user.displayName,
-          email: user.email,
-          localId: user.uid
-        })
-        const ref = db.collection('users').doc(user.uid)
-        ref.get().then(function (doc) {
-          commit('SET_PROFILE',
-            doc.data()
-          )
-        })
-      } else {
-        commit('SET_USER', null)
-        commit('SET_PROFILE', null)
-      }
-    },
-    fetchProfile ({ commit }, userUID) {
-      const ref = db.collection('users').doc(userUID)
-      ref.get().then(function (doc) {
-        commit('SET_PROFILE',
-          doc.data()
-        )
-      })
-    }
-  }
 })
